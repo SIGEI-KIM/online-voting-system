52054e533122daf3467a7eb52e9eeb2eeeee5e51
Enock Kimutai Sigei <esigei091@gmail.com>
2025-05-26 12:14:19 +0300

    reverted console

--- routes/console.php
+++ routes/console.php
@@ -1,22 +1,8 @@
 <?php
 
-
-use App\Console\Commands\UpdateElectionStatus;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command(UpdateElectionStatus::class)->daily();
-
-// You can define other console commands here using Artisan::command(...)
-
-// use Illuminate\Foundation\Inspiring;
-// use Illuminate\Support\Facades\Artisan;
-
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote');
